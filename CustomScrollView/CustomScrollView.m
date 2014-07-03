68fd6bccb503a7233df81a03343e12309a7539fa
Arkadiusz Holko <fastred@fastred.org>
2014-07-03 21:06:08 +0200

    Add Dynamics based deceleration

--- CustomScrollView/CustomScrollView.m
+++ CustomScrollView/CustomScrollView.m
@@ -4,9 +4,17 @@
 //
 //  Created by Ole Begemann on 16.04.14.
 //  Copyright (c) 2014 Ole Begemann. All rights reserved.
-//
+//  Parts of the class are based on https://github.com/grp/CustomScrollView/blob/custom-scroll-with-pop/CustomScrollView/CustomScrollView.m
 
 #import "CustomScrollView.h"
+#import "CSCDynamicItem.h"
+
+@interface CustomScrollView ()
+@property CGRect startBounds;
+@property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, weak) UIDynamicItemBehavior *decelerationBehavior;
+@property (nonatomic, strong) CSCDynamicItem *dynamicItem;
+@end
 
 @implementation CustomScrollView
 
@@ -36,25 +44,89 @@
 {
     UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
     [self addGestureRecognizer:panGestureRecognizer];
+
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
+    self.dynamicItem = [[CSCDynamicItem alloc] init];
 }
 
 - (void)handlePanGesture:(UIPanGestureRecognizer *)panGestureRecognizer
 {
-    CGPoint translation = [panGestureRecognizer translationInView:self];
-    CGRect bounds = self.bounds;
-    
-    CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
-    CGFloat minBoundsOriginX = 0.0;
-    CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
-    bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
+    switch (panGestureRecognizer.state) {
+        case UIGestureRecognizerStateBegan:
+        {
+            self.startBounds = self.bounds;
+            [self.animator removeAllBehaviors];
+        }
+            // fall through
 
-    CGFloat newBoundsOriginY = bounds.origin.y - translation.y;
-    CGFloat minBoundsOriginY = 0.0;
-    CGFloat maxBoundsOriginY = self.contentSize.height - bounds.size.height;
-    bounds.origin.y = fmax(minBoundsOriginY, fmin(newBoundsOriginY, maxBoundsOriginY));
+        case UIGestureRecognizerStateChanged:
+        {
+            CGPoint translation = [panGestureRecognizer translationInView:self];
+            CGRect bounds = self.startBounds;
 
-    self.bounds = bounds;
-    [panGestureRecognizer setTranslation:CGPointZero inView:self];
+            if (!self.scrollHorizontal) {
+                translation.x = 0.0;
+            }
+            if (!self.scrollVertical) {
+                translation.y = 0.0;
+            }
+
+            CGFloat newBoundsOriginX = bounds.origin.x - translation.x;
+            CGFloat minBoundsOriginX = 0.0;
+            CGFloat maxBoundsOriginX = self.contentSize.width - bounds.size.width;
+            bounds.origin.x = fmax(minBoundsOriginX, fmin(newBoundsOriginX, maxBoundsOriginX));
+
+            CGFloat newBoundsOriginY = bounds.origin.y - translation.y;
+            CGFloat minBoundsOriginY = 0.0;
+            CGFloat maxBoundsOriginY = self.contentSize.height - bounds.size.height;
+            bounds.origin.y = fmax(minBoundsOriginY, fmin(newBoundsOriginY, maxBoundsOriginY));
+
+            self.bounds = bounds;
+        }
+            break;
+        case UIGestureRecognizerStateEnded:
+        {
+            CGPoint velocity = [panGestureRecognizer velocityInView:self];
+            if (![self scrollHorizontal]) {
+                velocity.x = 0;
+            }
+            if (![self scrollVertical]) {
+                velocity.y = 0;
+            }
+            velocity.x = -velocity.x;
+            velocity.y = -velocity.y;
+
+            self.dynamicItem.center = self.bounds.origin;
+            UIDynamicItemBehavior *decelerationBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.dynamicItem]];
+            [decelerationBehavior addLinearVelocity:velocity forItem:self.dynamicItem];
+            decelerationBehavior.resistance = 2.0;
+
+            __weak typeof(self)weakSelf = self;
+            decelerationBehavior.action = ^{
+                // IMPORTANT: If the deceleration behavior is removed, the bounds' origin will stop updating. See other possible ways of updating origin in the accompanying blog post.
+                CGRect bounds = weakSelf.bounds;
+                bounds.origin = weakSelf.dynamicItem.center;
+                weakSelf.bounds = bounds;
+            };
+
+            [self.animator addBehavior:decelerationBehavior];
+            self.decelerationBehavior = decelerationBehavior;
+        }
+            break;
+
+        default:
+            break;
+    }
+}
+
+- (BOOL)scrollVertical
+{
+    return self.contentSize.height > CGRectGetHeight(self.bounds);
+}
+
+- (BOOL)scrollHorizontal
+{
+    return self.contentSize.width > CGRectGetWidth(self.bounds);
 }
 
 @end
